74a3d349aa88dc6a7f064b773975457de5571863
khanld <51778162+khanld@users.noreply.github.com>
2019-08-08 21:34:07 +0700

    fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //mongodb+srv://taskapp:unehihi12@cluster0-1zqtq.mongodb.net/webwraping?retryWrites=true&w=majority
 
-mongoose.connect('mongodb+srv://taskapp:unehihi12@cluster0-1zqtq.mongodb.net/webwraping?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://taskapp:unehihi12@cluster0-1zqtq.mongodb.net/film-api?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
