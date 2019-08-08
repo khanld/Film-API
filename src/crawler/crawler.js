0e5bc5006d6150ec246820e1fbcb67a8fceb66a4
khanld <51778162+khanld@users.noreply.github.com>
2019-08-08 22:19:12 +0700

    fix

--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -15,10 +15,14 @@ const filmImageCrawler = (film) => {
 const filmContentCrawler = async () => {
 	try {
 			await Film.deleteMany()
-			const browser = await puppeteer.launch({headless: true, defaultViewport: {
-			width: 1280,
-			height: 568
-		}})
+			const browser = await puppeteer.launch({
+			headless: true, 
+			defaultViewport: {
+				width: 1280,
+				height: 568
+			},
+			args: ['--no-sandbox','--disable-setuid-sandbox']
+		})
 
 		const page = await browser.newPage()
 		await page.goto('https://fimfast.com/phim-le', {
