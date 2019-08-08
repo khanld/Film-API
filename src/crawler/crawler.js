e33f9c51251d6719d7c8a0a974fb9443b893ef6e
khanld <51778162+khanld@users.noreply.github.com>
2019-08-08 22:55:22 +0700

    fix

--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -21,7 +21,10 @@ const filmContentCrawler = async () => {
 				width: 1280,
 				height: 568
 			},
-			args: ['--no-sandbox', '--disable-setuid-sandbox']
+			args: [ '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--single-process']
 		})
 
 		const page = await browser.newPage()
