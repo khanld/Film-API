280642d2ef20d44f9aed3b093d1dcee2cd715002
khanld <51778162+khanld@users.noreply.github.com>
2019-08-08 21:47:50 +0700

    fix

--- src/cronjob/cron.js
+++ src/cronjob/cron.js
@@ -1,6 +1,8 @@
 const CronJob = require('cron').CronJob
 const filmContentCrawler = require('../crawler/crawler')
 
-const cron = new CronJob('0 */5 * * * *', filmContentCrawler)
+const cron = new CronJob('0 */5 * * * *', () => {
+	filmContentCrawler()
+})
 
 module.exports = cron
