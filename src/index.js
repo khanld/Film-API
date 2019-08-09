cf6fc5c335aaf9ff7dea892b1f87928bd4b44e0e
khanld <51778162+khanld@users.noreply.github.com>
2019-08-09 13:10:54 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -14,11 +14,6 @@ app.get('/', async (req, res) => {
 	try {
 
 			const browser = await puppeteer.launch({
-			headless: true, 
-			defaultViewport: {
-				width: 1280,
-				height: 568
-			},
 			args: ['--no-sandbox', '--disable-setuid-sandbox']
 		})
 
