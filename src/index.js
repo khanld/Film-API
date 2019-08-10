98812ef2973b31905572b608b818076d99ec392a
khanld <51778162+khanld@users.noreply.github.com>
2019-08-10 19:17:41 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT 
-const puppeteer = require('puppeteer-core')
+const puppeteer = require('puppeteer')
 
 
 app.get('/', async (req, res) => {
@@ -13,52 +13,18 @@ app.get('/', async (req, res) => {
 		})
 
 		const page = await browser.newPage()
-		await page.goto('https://fimfast.com/phim-le', {
+		await page.goto('https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md', {
 			waitUntil: ['domcontentloaded', 'networkidle0']
 		})
+		console.log("hello")
 
-		
-		//Turn off the popup form
-		const frameSelector = await page.$('#ur-render-target')
-		if (frameSelector) {
-			const frame = await frameSelector.contentFrame()
-			const but = await frame.$('#btn-reject')
-			await but.click()
-		}
-
-
-		const films = await page.evaluate(() => {
-			let filmEndpointList = document.querySelectorAll('.tray-item > a')
-			let filmQualityList = document.querySelectorAll('.tray-item-quality')
-			let filmTitleList = document.querySelectorAll('.tray-item-title')
-			let filmDurationList = document.querySelectorAll('.tray-item-title')
-			let filmPointList = document.querySelectorAll('.tray-item-point')
-			let filmImageUrl = document.querySelectorAll('.tray-item-thumbnail')
-
-			filmPointList = [...filmPointList]
-			filmEndpointList = [...filmEndpointList]
-			filmQualityList = [...filmQualityList]
-			filmDurationList = [...filmDurationList]
-			filmTitleList = [...filmTitleList]
-			filmImageUrl = [...filmImageUrl]
-		
-			const films = filmTitleList.map((filmTitle, index) => {
-				return {
-					title: filmTitle.textContent,
-					point: filmPointList[index] !== undefined? filmPointList[index].textContent: undefined,
-					quality: filmQualityList[index].textContent,
-					duration: filmDurationList[index].textContent,
-					endpoint: filmEndpointList[index].getAttribute('href'),
-					imageUrl: filmImageUrl[index].getAttribute('src')
-				}
-			})
-			
-			return films
+		const title = await page.evaluate(() => {
+			let title = document.querySelector('h1')
+			return title.textContent
 		})
+		console.log(title)
 
-
-		console.log(films.length)
-		res.send(films)
+		res.send(title)
 		return await browser.close()
 
 	} catch (e) {
